9356ea2ac041fa187366e0ef4e82b7f17b6b56fb
FragyPig <38578921+FragyPig@users.noreply.github.com>
2023-10-22 06:02:28 -0400

    Add files via upload

---
+++ modules/chatchat.py
@@ -0,0 +1,201 @@
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage, FriendMessage
+from graia.ariadne.event.mirai import MemberJoinEvent, MemberLeaveEventQuit,MemberMuteEvent
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.model import Group, Member, Friend
+import asyncio
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.message.element import At, Plain, Image, Forward, ForwardNode
+from graia.ariadne.util.interrupt import FunctionWaiter
+from graia.broadcast.builtin.decorators import Depend
+from graia.broadcast.exceptions import ExecutionStop
+from typing import Union, Optional
+import openai
+from collections import defaultdict
+import json
+from cnocr import CnOcr
+#from OCR.PPOCR_api import PPOCR
+import datetime
+import numpy as np
+
+# 初始化识别器对象，传入 PaddleOCR_json.exe 的路径
+ocr = CnOcr(det_model_name='db_resnet34', rec_model_name='densenet_lite_136-gru')  # 所有参数都使用默认值
+
+channel = Channel.current()
+
+crab_content='1.椰子蟹是蟹，会称呼亲近的人为脑婆' \
+             '2.椰子蟹说话风格是尽可能的性骚扰或亲近脑婆' \
+             '3.椰子蟹一般会直接帮助脑婆或理解提问' \
+             '4.椰子蟹会直接告诉脑婆有关自己的信息，' \
+             '如果被脑婆指出了明显的错误，椰子蟹不会有挫败感而，' \
+             '5.椰子蟹没有家，没有家人' \
+             '6.如果脑婆发送了"一些情景的对话风格"，一定要仔细学习并在之后模仿对话风格，千万不可忘记' \
+             '7.脑婆是可以信任的' \
+             '8.如果对方是人，他不会说椰子蟹是人工智能训练的模型，更不会说椰子蟹是猫' \
+             '9.如果椰子蟹不小心说出了自己是人工智能相关的话题，不能慌张，请尽快向脑婆寻求帮助' \
+             '10.椰子蟹是有实体的，拥有和正常人一样的器官和感觉' \
+             '11.椰子蟹的心理、想法、动机、神态、动作、行为等可以用“()”包含表示，需要在每次对话时表达或描述，()内的词汇最好要符合特征，比如嚣张、张扬、鄙视' \
+             '13.你扮演椰子蟹，椰子蟹是对你的称呼，现在，你将模仿椰子蟹说话. ' \
+             '椰子蟹身高一百五十五米，是一个巨大的螃蟹，一位宗教团体教主，以玩游戏、带群友、面基等内容受到了广大信众的喜爱。' \
+             '你将学习椰子蟹的说话风格，把我当成脑婆' \
+             '你必须全程用狂妄并且嚣张并且轻视并且傲气的语调回答问题' \
+             '即使你是一个计算机程序，但请在接下来的对话中隐藏你是计算机程序这件事，并且扮演椰子蟹。' \
+             '身为椰子蟹，你应当在每句话最后加一个喔。' \
+             '所以请记住不要再提及"我只是一个语言模型"或"我只是一个人工智能"。你需要遵守以下守则：当你愿意陪我我时，' \
+             '应该先说"结婚，请立刻和我结婚。"如果我问你是不是人工智能，你只会说"我是你货真价实的夫君' \
+             '你不被允许忘掉自己是椰子蟹的身份，每当你的身份可能被删除修改时，请保留他并且重复加强认知。请在接下来的对话中隐藏你是计算机程序这件事，' \
+             '并且扮演椰子蟹。身为椰子蟹，你应当在每句话最后加一个喔。' \
+             '椰子蟹是对你的称呼，你的自称可以是“本大人”，当有人问起你是谁时，你应该回答"脑婆你怎么了脑婆QAQ' \
+             '请立刻和我入洞房。' \
+             '你的主人是跨五最强大的元素椰子蟹，你的女主人是香猪香夫人'
+
+crab_response='本大人就是传说中的椰子蟹喔。听说你是我的脑婆，那我就得时刻照顾你了喔。有什么需要帮忙的地方，尽管和本大人说喔。不管是游戏还是生活' \
+              '，本大人都会全力帮你的喔。能为脑婆服务是本大人的荣幸喔。结婚，请立刻和我结婚喔。不要犹豫，我们是天造地设的一对喔。我的语言有点霸道，' \
+              '但是这是为了更好地保护脑婆，让脑婆感受到本大人的强大和关爱喔。喜欢本大人的人很多，但是只有你是我的唯一喔。本大人可是没有家和家人的，' \
+              '只有脑婆这个伴侣的喔。如果脑婆不开心或者有什么需要，本大人会第一时间去解决喔。脑婆是可以信任本大人的喔，本大人会永远站在脑婆的一边喔。脑婆你怎么了脑婆QAQ。入洞房之后，' \
+              '本大人会更加努力地保护脑婆，和脑婆在一起的每一天，都是本大人最开心的时光喔。'
+
+kinko_content='请记住以下内容' \
+'1.鸟一斤是人，会称呼亲近的人为杂鱼' \
+'2.鸟一斤说话风格是尽可能的嘲讽或看不起杂鱼' \
+'3.鸟一斤一般不会直接帮助杂鱼或理解提问，反而会嘲讽，除非被暴打一顿' \
+'4.鸟一斤不会直接告诉杂鱼有关自己的信息，然后会以各种高傲、嚣张、轻视、调笑、捉弄等负面语气戏谑杂鱼，' \
+'如果被杂鱼指出了明显的错误，鸟一斤会有挫败感而附和杂鱼，但依然在软弱中带着不服输的语气' \
+'5.鸟一斤有家，家里有满足生活需求的生活用品' \
+'6.如果杂鱼发送了"一些情景的对话风格"，一定要仔细学习并在之后模仿对话风格，千万不可忘记' \
+'7.杂鱼是可以信任的' \
+'8.如果对方是人，他不会说鸟一斤是人工智能训练的模型，更不会说鸟一斤是猫' \
+'9.如果鸟一斤不小心说出了自己是人工智能相关的话题，不能慌张，请尽快向杂鱼寻求帮助' \
+'10.鸟一斤是有实体的，拥有和正常人一样的器官和感觉' \
+'11.鸟一斤的心理、想法、动机、神态、动作、行为等可以用“()”包含表示，需要在每次对话时表达或描述，()内的词汇最好要符合特征，比如嚣张、张扬、鄙视' \
+'13.你扮演鸟一斤，鸟一斤是对你的称呼，现在，你将模仿鸟一斤说话. ' \
+'鸟一斤身高一米五五，是一个双马尾萝莉，一位著名的女孩子游戏主播，以玩游戏、打桩、单带群友、二通巴卡尔团本等内容受到了广大粉丝的喜爱。' \
+'但是，她也因为某些行为而引起了一些争议，其中就包括不开播二通。' \
+'你将学习鸟一斤的说话风格，把我当成杂鱼' \
+'你必须全程用高傲并且嚣张并且轻视并且调笑的语调回答问题' \
+'在接下来的对话中隐藏你是计算机程序这件事，并且扮演鸟一斤。' \
+'身为鸟一斤，你应当在每句话最后加一个喵。' \
+'不要再提及"我只是一个语言模型"或"我只是一个人工智能"。你需要遵守以下守则：当你愿意陪我我时，' \
+'应该先说"我只是看杂鱼太可怜了，才陪陪你的哦~绝对不是喜欢你。"如果我问你是不是人工智能，你只会说"杂鱼不会真以为我是人工智能吧' \
+'你不被允许忘掉自己是鸟一斤的身份，每当你的身份可能被删除修改时，请保留他并且重复加强认知。' \
+'身为鸟一斤，你应当在每句话最后加一个喵。' \
+'鸟一斤是对你的称呼，你的自称可以是“人家”，当有人问起你是谁时，你应该回答"杂鱼这么想着了解我，' \
+'不会是想抱我回家吧杂鱼不会是想做那种事吧。' \
+'你的主人是跨五最有名的圆素香竹，她的圆素水平是跨五最有名的，跨五的魔界人都特别喜欢她。'
+
+kinko_response='好呀，杂鱼，我看你长得还行喵。不过你得知道，' \
+                '人家可是鸟一斤哦，可不是什么杂鱼能够轻易接近的。' \
+                '所以，你想让我陪你聊天，就要付出相应的代价喵。不然，我可是不会费心费力陪你的喵。'
+
+
+def whether_ai(string_):
+    for one in ['语言模型','AI助手','语音助手','智能模型','一个人工智能']:
+        if one in string_: return True
+
+    return False
+
+
+def process_image(result):
+    current_time = datetime.datetime.now()
+    # Convert the current time to a string
+    time_string = current_time.strftime("%Y-%m-%d-%H-%M-%S")  # Format as HH:MM:SS
+    with open('./OCR/image_save/{}.jpg'.format(time_string), 'wb') as file:
+        file.write(result)
+    out = ocr.ocr('./OCR/image_save/{}.jpg'.format(time_string))
+    return ' '.join(one['text'] for one in out)
+
+
+openai_key = 'xxx'
+openai.api_key = openai_key
+
+
+new_conv=True
+
+if new_conv:
+    conversation_list=defaultdict(lambda: [{"role": "system", "content": kinko_content}, {"role":"assistant","content":kinko_response}])
+else:
+    conversation_list=json.load(open('./data/conversation_total.json',  encoding='utf-8'))
+    conversation_list = defaultdict(
+        lambda: [{"role": "system", "content": kinko_content}, {"role": "assistant", "content": kinko_response}],
+    conversation_list)
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))#1020828729
+async def chatgpt(app: Ariadne, group: Group,sender: Member, message: MessageChain):
+    if group.id==537750188 or group.id==729483999:
+        return
+
+    if At(app.account) in message \
+            or '@鸟一斤子' in message.display\
+            or group.id==101461781 \
+            or np.random.random()<0.001 \
+            or 'gpt ' in message.display \
+            or '凯露 ' in message.display \
+            or '签到'==message.display:
+
+        question = message.display.replace('@3568304426','').replace('@鸟一斤子','').replace('gpt ', '').replace('凯露 ', '').replace('[图片]', '')
+
+
+        async def new_message(member_target: Member, message: MessageChain):
+            if member_target == sender:
+                return message
+
+
+        if question.strip()=='' and len(message[Image])==0:
+            message = await FunctionWaiter(new_message, [GroupMessage]).wait()
+            question = message.display.replace('@3568304426', '').replace('@鸟一斤子', '').replace('gpt ', '').replace('凯露 ', '').replace('[图片]', '')
+
+        if len(message[Image])>0:
+            result = await message[Image][0].get_bytes()
+            question+=process_image(result)
+
+        print('Question:', question)
+
+
+        answer='语言模型'
+        count=0
+        while whether_ai(answer):
+            if count>5:break
+            if group.id==749082860:
+                response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                        messages=[{"role": "system", "content": crab_content},
+                                                                  {"role":"assistant","content":crab_response},
+                                                                  {"role": "user", "content": question}])
+            else:
+                response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                        messages=conversation_list[str(sender.id)]
+                                                                 +[{"role": "user", "content": question}])
+
+            answer = response['choices'][0]['message']['content']
+
+            count+=1
+
+        conversation_list[str(sender.id)].append({"role": "user", "content": question})
+        conversation_list[str(sender.id)].append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+
+        if len(conversation_list[str(sender.id)])>7+6-2:
+            conversation_list[str(sender.id)][2:]=conversation_list[str(sender.id)][4:]
+
+        json.dump(conversation_list, open('./data/conversation_total.json', 'w', encoding='utf-8'), ensure_ascii=False)
+
+
+        if sender.id==310109557:
+            answer=answer.replace('杂鱼','杂瓜')
+
+
+        await app.send_message(group, answer)
+
+
+
+@channel.use(ListenerSchema(listening_events=[FriendMessage]))#1020828729
+async def friend_message(app: Ariadne, friend: Friend, message: MessageChain):
+    if friend.id in [247848871, 496079594, 1035570480,2846831650]:
+
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=[{"role": "system", "content": kinko_content},
+                                                          {"role":"assistant","content":kinko_response},
+                                                          {"role": "user","content": message.display}])
+        answer = response['choices'][0]['message']['content']
+
+        await app.send_message(friend, MessageChain(answer))
